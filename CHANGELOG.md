d416db044e5dbc6e698e86607a5c1583d683c1b0
Pascal Stark <CRK@zurich.ibm.com>
2020-01-28 18:18:14 +0100

    Version 1.0.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,8 @@
 # Changelog
 
 All notable changes to this project will be documented in this file.
+
+Version 1.0.0  (1/28/2020)
+--------------------------
+Initial release
+
--- setup.py
+++ setup.py
@@ -1,17 +1,35 @@
+# +-----------------------------------------------------------------------------+
+# |  Copyright 2019-2020 IBM Corp. All Rights Reserved.                                       |
+# |                                                                             |
+# |  Licensed under the Apache License, Version 2.0 (the "License");            |
+# |  you may not use this file except in compliance with the License.           |
+# |  You may obtain a copy of the License at                                    |
+# |                                                                             |
+# |      http://www.apache.org/licenses/LICENSE-2.0                             |
+# |                                                                             |
+# |  Unless required by applicable law or agreed to in writing, software        |
+# |  distributed under the License is distributed on an "AS IS" BASIS,          |
+# |  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.   |
+# |  See the License for the specific language governing permissions and        |
+# |  limitations under the License.                                             |
+# +-----------------------------------------------------------------------------+
+# |  Authors: Lorenz K. Mueller, Pascal Stark                                   |
+# +-----------------------------------------------------------------------------+
+
 import setuptools
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="ComplexLinearNetworkAnalyzer",
-    version="0.0.1",
+    name="complex-linear-network-analyzer",
+    version="1.0.0",
     author="Lorenz K. Müller, Pascal Stark",
     author_email="crk@zurich.ibm.com",
-    description=" Computes analytic output of complex valued, linear networks.",
+    description=" Computes analytically the output of complex valued, linear networks.",
     long_description=long_description,
     long_description_content_type="text/x-rst",
-    url="https://github.ibm.com/lmu-zurich/analyticalReservoir",
+    url="https://github.com/IBM/complex-linear-network-analyzer",
     packages=['colna'],
     install_requires=['tqdm','scipy','numpy','matplotlib'],
     extras_require={
